d0a37796d426144c2f86cd458ff1532c41c80824
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-08-29 16:34:29 +0000

    JS Primitives and Variables completed

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+var fullName = 'Louisa Whitaker';
+var isCool = true;
+var totalPets = 1;
+
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', totalPets);
+
+var unknown = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+var never;
+console.log('never:', never);
+console.log('typeof never:', never);
