60a2c2618e919b40097d8f86b185d9bc3587feb8
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-08-29 16:38:18 +0000

    JS Primitives and Variables, fixed labels

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -2,16 +2,16 @@ var fullName = 'Louisa Whitaker';
 var isCool = true;
 var totalPets = 1;
 
-console.log('fullName:', fullName);
-console.log('isCool:', isCool);
-console.log('totalPets:', totalPets);
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
 console.log('typeof fullName:', typeof fullName);
 console.log('typeof isCool:', typeof isCool);
-console.log('typeof totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
 
 var unknown = null;
-console.log('unknown:', unknown);
+console.log('value of unknown:', unknown);
 console.log('typeof unknown:', typeof unknown);
 var never;
-console.log('never:', never);
-console.log('typeof never:', never);
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
