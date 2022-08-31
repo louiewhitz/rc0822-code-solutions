cc48332d9edb2cd186b62ca7e5dfca7b9914ec6c
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-08-31 19:35:55 +0000

    js if

---
+++ javascript-if/main.js
@@ -0,0 +1,75 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'j' || string[0] === 'J') {
+    return true;
+  }
+  return false;
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age <= 21) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else
+  if ((pH <= 7) && (pH >= 0)) {
+    return 'acid';
+  } else
+  if (pH <= 14 && pH >= 7) {
+    return 'base';
+  } else {
+    return 'invalid ph level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  if ((name === 'yakko') || (name === 'wakko')) {
+    return 'We\'re the Warner Bro\'s';
+  } else if (name === 'dot') {
+    return 'I\'m cute';
+  } else {
+    return 'Goodnight Everybody!';
+  }
+}
