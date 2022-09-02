2dcd4237fc7c4a07e5d22138e6cc445c32d21787
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-02 00:04:39 +0000

    string-man 1 begin

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,8 @@
+/* exported getFirstChar */
+function getFirstChar(string) {
+  var char = string[0];
+  return char;
+}
+// find the first element in the string
+// store it in a variable
+// return that variable
