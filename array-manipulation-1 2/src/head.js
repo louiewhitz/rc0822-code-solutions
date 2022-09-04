9fa51b1125d4dfcefaf6c18538031a8bff3c2401
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-04 21:19:41 +0000

    Array-man-1

---
+++ array-manipulation-1 2/src/head.js
@@ -0,0 +1,9 @@
+/* exported head */
+function head(array) {
+  var first = array[0];
+  return first;
+}
+
+// takes the first index value of the array
+// find the first index by storing the array[0] in a variable
+// return the variable
