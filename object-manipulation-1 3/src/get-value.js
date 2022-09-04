739361de22d98ebe06c09354f70139649214522c
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-04 21:51:27 +0000

    Obj Man 1

---
+++ object-manipulation-1 3/src/get-value.js
@@ -0,0 +1,8 @@
+/* exported getValue */
+function getValue(object, key) {
+
+  return object[key];
+
+}
+
+// find the value of key by using the object at key method
---
+++ object-manipulation-1 3/src/set-value.js
@@ -0,0 +1,7 @@
+/* exported setValue */
+function setValue(object, key, value) {
+  object[key] = value;
+
+}
+
+// set the value of the key in a an object
