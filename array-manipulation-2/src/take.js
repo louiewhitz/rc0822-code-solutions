7db4c225fff3d6fb4b421eb2aa5b6fba8e759946
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-07 16:24:20 +0000

    array-man-2 almost finished

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,8 @@
 /* exported take */
+function take(array, count) {
+  // create an variable to store the new values after using the slice method
+  // store count as the number that should be taken from the beginning of the array
+
+  var newArr = array.slice(0, count);
+  return newArr;
+}
