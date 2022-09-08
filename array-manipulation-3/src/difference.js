9e0e5ed4689ced0e720852d29e4f65969b5eed96
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-08 17:00:25 +0000

    array 3 - difference test

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,33 @@
 /* exported difference */
+function difference(first, second) {
+  var arr = [];
+  for (var i = 0; i < first.length; i++) {
+    var firstWords = first[i];
+    arr.push(firstWords);
+
+    // console.log('~ firstWords', firstWords);
+
+  }
+
+  for (var j = 0; j < second.length; j++) {
+    var secondWords = second[j];
+    arr.push(secondWords);
+
+    // console.log('~ secondWords', secondWords);
+  }
+  for (var x = 0; x < arr.length; x++) {
+    // var char = arr[x];
+    // for (var x = 0; );
+
+  }
+
+  // if (firstWords !== secondWords) {
+  //   arr.push(firstWords);
+  // }
+  // if (secondWords !== firstWords) {
+  //   arr.push(secondWords);
+  //   console.log(arr);
+  // }
+
+  return arr;
+}
