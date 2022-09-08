3ae334f19dea21fc13fae9c569b8d4876c870927
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-08 03:47:19 +0000

    JS Typing Tutor draft for review!

--- javascript-typing-tutor 2/main.js
+++ javascript-typing-tutor 2/main.js
@@ -1,6 +1,4 @@
 var allSpan = document.querySelectorAll('span');
-// var textBox = document.querySelector('.input');
-// var inputNode = textBox.childNodes;
 
 var current = 0;
 
@@ -8,16 +6,19 @@ document.addEventListener('keydown', keyDown);
 
 function keyDown(event) {
   var pressedKey = event.key;
+
   if (pressedKey === allSpan[current].textContent) {
     current++;
-
     allSpan[current - 1].className = 'span correct';
-    if (allSpan[current].textContent !== pressedKey) {
+    // allSpan[current].className = 'span active';
+    var nextChar = allSpan[current + 1];
+    if (nextChar !== true) {
       allSpan[current].className = 'span active';
     }
+
   } else if (pressedKey !== allSpan[current]) {
-    allSpan[current - 1].className = 'span incorrect';
-    allSpan[current].className = 'span';
+    allSpan[current].className = 'span incorrect';
+
     // current++;
     if (allSpan[current - 1] === pressedKey && allSpan[current - 1] === true) {
       allSpan[current - 1].className = 'span correct';
