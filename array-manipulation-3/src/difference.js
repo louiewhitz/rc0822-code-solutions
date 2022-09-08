9b91df4772db494c6825dcd1e220c76ea752bacf
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-08 19:12:38 +0000

    array 3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,6 +1,8 @@
 /* exported difference */
 function difference(first, second) {
+  // create an empty final array
   var arr = [];
+  var final = [];
   for (var i = 0; i < first.length; i++) {
     var firstWords = first[i];
     arr.push(firstWords);
@@ -10,16 +12,23 @@ function difference(first, second) {
   }
 
   for (var j = 0; j < second.length; j++) {
-    var secondWords = second[j];
-    arr.push(secondWords);
+    // var secondWords = second[j];
+  //   if (first.indexOf(secondWords) === -1) {
+  //  }
 
     // console.log('~ secondWords', secondWords);
   }
-  for (var x = 0; x < arr.length; x++) {
-    // var char = arr[x];
-    // for (var x = 0; );
+  // console.log('arr', arr);
+  // for (var x = 0; x < arr.length; x++) {
+  //   var char = arr[x];
+  //   for (var y = 0; y < arr.length; y++) {
+  //     if (!final.includes(char)) {
+  //       final.push(char);
 
-  }
+  //       console.log('~ final', final);
+  //     }
+  //   }
+  // }
 
   // if (firstWords !== secondWords) {
   //   arr.push(firstWords);
@@ -29,5 +38,5 @@ function difference(first, second) {
   //   console.log(arr);
   // }
 
-  return arr;
+  return final;
 }
