11387db72bc429d1bdeb362083c0309f3c5b3030
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-09 03:11:55 +0000

    Obj man 2 begin

---
+++ object-manipulation-3 2/main.js
@@ -0,0 +1,27 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// create collection of players
+// var player1 = {
+//   name: 'Jay',
+//   hand: []
+// };
+
+// var player2 = {
+//   name: 'Allen',
+//   hand: []
+// };
+
+// var player3 = {
+//   name: 'Lucy',
+//   hand: []
+// };
+
+// var player4 = {
+//   name: 'Max',
+//   hand: []
+// };
+// create 52card deck
+// var pointArray = [11, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+// var suitArray = ['clubs', 'diamonds', 'spade', 'heart'];
+// var rankArray = ['Ace', 'King', 'Queen', 'Jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two'];
+
+// create point system array;
