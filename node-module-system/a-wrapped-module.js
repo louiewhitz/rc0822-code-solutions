f2e2001a4219b8e629c6e4cf11b369d3ee389886
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-12 17:28:57 +0000

    node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,13 @@
+console.log('typeof exports', typeof exports);
+console.log('exports', exports);
+console.log('typeof require', typeof require);
+console.log('require', require);
+
+console.log('~ typeof module', typeof module);
+console.log('~ module', module);
+
+console.log('~ typeof _filename', typeof __filename);
+console.log('~ _filename', __filename);
+
+console.log('~ typeof __dirname', typeof __dirname);
+console.log('~ __dirname', __dirname);
