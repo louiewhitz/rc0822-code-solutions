0cb480be0801eca6f57c397cda990f13d354b779
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-12 00:04:12 +0000

    Node process argv

---
+++ node-process-argv/all-arguments.js
@@ -0,0 +1,8 @@
+// process.argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+
+// console.log(process.argv);
+console.log(process.argv);
+console.log('process.argv[0]:', process.argv[0]);
+console.log('process.argv[1]:', process.argv[1]);
