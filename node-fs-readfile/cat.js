ebd5f324b7acadac67b827a7816a255ac4cc1355
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-13 19:22:59 +0000

    practicing cat.js

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,9 @@
+// const fs = require('fs');
+
+// function chunck(filename, cb) {
+//   fs.readFile(filename, cb);
+// }
+//  let file = process.argv[1];
+//  fs.readFile(file, process.argv[2], 'utf8', (data, err) =>{}) {
+
+//  }
