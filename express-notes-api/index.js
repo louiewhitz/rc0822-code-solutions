fe5bf24a0ccaf10919e0b558650700519573fe12
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-19 20:59:50 +0000

    removed console.logs

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,43 +1,28 @@
 /* eslint-disable no-console */
 const express = require('express');
 const json = require('./data.json');
-// console.log('json ', json);
-
-// const read = function read() {
-//   for (const key in json.notes) {
-//     const entryNumbers = key;
-//     const entryNote = json.notes[key];
-//     console.log('this is fs.read', `${entryNumbers}: ${entryNote}`);
-//   }
-// };
 const app = express();
 
 const fs = require('fs');
-
-// fs.readFile(json)
 
 app.get('/api/notes', (req, res) => {
   const notesArr = [];
   for (const id in json.notes) {
     notesArr.push(json.notes[id]);
     console.log(notesArr);
-    // if(notesArr.length < 0)
+
   }
   res.status(200);
   res.json(notesArr);
 });
-// const oneNote = {};
-// const fs = require('fs');
+
 app.get('/api/notes/:id', (req, res) => {
   const oneId = Number(req.params.id);
-
   if (isNaN(oneId) === true || oneId < 1) {
     res.status(400).json({ ERROR: 'ID must be a positive number' });
   } else if (json.notes[oneId] === undefined) {
     res.status(404).json({ ERROR: 'Sorry, there is no ID by that number' });
   } else {
-    console.log('json.notes[oneId]', json.notes[oneId]);
-
     res.status(200).json(json.notes[oneId]);
   }
 });
@@ -123,8 +108,6 @@ app.put('/api/notes/:id', (req, res) => {
     json.notes[oneId].content = editNote;
     const updated = json.notes[oneId];
 
-    // updated.id = oneId;
-    // json.notes[oneId].content = updated;
     const editJson = JSON.stringify(json, null, 2);
 
     fs.writeFile('data.json', editJson, 'utf8', err => {
@@ -138,13 +121,6 @@ app.put('/api/notes/:id', (req, res) => {
     });
   }
 });
-// err => {
-//     if (err) {
-//       console.error(err);
-//       res.sendStatus(500).json({ ERROR: 'an unexpected error occurred' });
-//     }
-
-// });
 
 app.listen(3000, () => {
   console.log('server 3000');
