ad8e725985a8158e9aa3d3892d477c86cb766bf8
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-21 02:34:33 +0000

    sql join

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,10 @@
+select
+  "films"."title" as "film title",
+  "films"."releaseYear",
+  "genres"."name" as "genre name"
+from
+  "films"
+  join "filmGenre" using ("filmId")
+  join "genres" using ("genreId")
+where
+  "films"."title" = 'Boogie Amelie';
