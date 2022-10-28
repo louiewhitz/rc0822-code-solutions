08e6aa7c20a4ae432ec4fbc0a3688733bda346d2
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-28 23:00:58 +0000

    react validated input

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      isValid: false,
+      errorMessage: 'Please enter a password'
+    };
+
+    this.tooLittleChar = this.tooLittleChar.bind(this);
+  }
+
+  tooLittleChar(event) {
+    this.setState({ password: event.target.value });
+    const password = event.target.value;
+    if (password.length < 7) {
+      this.setState({
+        password: event.target.value,
+        isValid: false
+      });
+    } else {
+      this.setState({ isValid: true });
+    }
+  }
+
+  render() {
+    let text = '';
+    let icon = '';
+    let color = '';
+
+    if (this.state.password.length === 0) {
+      text = 'Please enter a password';
+      icon = 'fa-sold fa-x';
+      color = 'wrong';
+    } else if (
+      this.state.password.length < 8
+    ) {
+      icon = 'fa-sold fa-x';
+      color = 'wrong';
+      text = 'Please enter up to 8 characters';
+    } else {
+      text = '';
+      icon = 'fa-solid fa-check';
+      color = 'correct';
+    }
+
+    return (
+      <form>
+        <label htmlFor="password">Password</label>
+        <input type="password" onChange={ this.tooLittleChar }/>
+        <i className={icon}></i>
+        <p className={color}>{text}</p>
+      </form>
+    );
+  }
+}
+
+export default ValidatedInput;
