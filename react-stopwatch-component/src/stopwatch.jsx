e2f84c3b7c31d6772ed26111ffff327fde5ce20c
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-10-28 16:05:09 +0000

    started stopwatch

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StopWatchComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // setInterval(handleInterval, delay)
+    };
+  }
+}
+
+export default StopWatchComponent;
