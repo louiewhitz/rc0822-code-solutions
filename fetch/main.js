2e1d0995081a17ba8ada6e221fc4c19f215eb2d9
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-11-01 19:22:06 +0000

    added second part of the exercise

--- fetch/main.js
+++ fetch/main.js
@@ -2,3 +2,7 @@ const url = 'https://jsonplaceholder.typicode.com/users';
 fetch(url)
   .then(response => response.json())
   .then(data => console.log(data));
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(res => res.json())
+  .then(bulbasaur => console.log(bulbasaur));
